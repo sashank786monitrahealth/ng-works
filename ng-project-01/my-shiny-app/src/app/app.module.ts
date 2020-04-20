cd1248e57bdf8a9985858a2352df9a83664282cd
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-04-20 17:29:37 +0530

    added app-works

--- ng-project-01/my-shiny-app/src/app/app.module.ts
+++ ng-project-01/my-shiny-app/src/app/app.module.ts
@@ -7,13 +7,14 @@ import {CoreModule} from './core/core.module';
 import { UserComponent } from './user/user.component';
 import { TransformDirective } from './transform.directive';
 import { SafePipe } from './pipes/safe/safe.pipe';
+import { UserService } from './user/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     TransformDirective,
-    SafePipe
+    SafePipe,
   ],
   imports: [
     BrowserModule,
