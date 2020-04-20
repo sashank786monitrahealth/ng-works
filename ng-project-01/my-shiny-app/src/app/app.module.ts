def470330131d8a9b61d1bd400e2f5860ca93c6a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-04-20 15:06:47 +0530

    ng g c user

--- ng-project-01/my-shiny-app/src/app/app.module.ts
+++ ng-project-01/my-shiny-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CoreModule} from './core/core.module';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
